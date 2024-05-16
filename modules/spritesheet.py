64507defe280bc83bab28d1fe1bebfd221f573c2
andre <andrey19080308-12@mail.ru>
2024-05-16 15:39:42 +0500

    Убрал лишние методы и переменные

--- modules/spritesheet.py
+++ modules/spritesheet.py
@@ -1,6 +1,6 @@
 """
 author: Al0n1
-version: 0.0.2
+version: 0.0.3
 
 :description:
 Модуль содержит класс листа спрайтов. Этот класс содержит функции позволяющие выделить из листа
@@ -16,7 +16,7 @@ class SpriteSheet:
     """
     Класс последовательности(лист) спрайтов.
     """
-    def __init__(self, filename: str, state: str = "idle"):
+    def __init__(self, filename: str):
         """
         :param filename: название файла последовательности
         :param state: состояние объекта на спрайте
@@ -26,11 +26,7 @@ class SpriteSheet:
         with open("../Game/sprites/sprite_sheet.json", "r") as file:
             filename = filename[:filename.find('_')]
             self.__data = json.load(file)['frames'][filename]
-        self.__last_tick = pygame.time.get_ticks()
-        self.__cooldown = 100
-        self.__state = state
 
-    # <editor-fold desc="Функции для получения спрайта">
     def _get_sprite(self, x: int, y: int, w: int, h: int, scale: int) -> pygame.Surface:
         """
         Функция создаёт поверхность pygame и выводит на неё спрайт
@@ -47,42 +43,21 @@ class SpriteSheet:
         sprite = pygame.transform.scale(sprite, (w * scale, h * scale))
         return sprite
 
-    def parse_sprite(self, sprite_index: int = 1, scale: int = 1) -> pygame.Surface:
+    def parse_sprite(self, state: str, sprite_index: int = 1, scale: int = 1, ) -> pygame.Surface:
         """
         Функция находит нужный спрайт и его параметры в метаданных
+        :param state: состояние монстра для отображения
         :param sprite_index: номер слайда в последовательности
         :param scale: множитель размера спрайта
         :return: поверхность pygame со спрайтом
         """
-        sprite = self.__data[self.__state][f'frame_{sprite_index}']['frame']
+        sprite = self.__data[state][f'frame_{sprite_index}']['frame']
         x, y, w, h = sprite["x"], sprite["y"], sprite["w"], sprite["h"]
         image = self._get_sprite(x, y, w, h, scale)
         return image
-    # </editor-fold>
 
-    def can_change_sprite(self) -> bool:
-        """
-        Функция проверяет прошло ли достаточно времени для смены кадра спрайта
-        :return: True/False
-        """
-        now = pygame.time.get_ticks()
-        if now - self.__last_tick >= self.__cooldown:
-            self.__last_tick = now
-            return True
-        else:
-            return False
-
-    # <editor-fold desc="Функции и методы для работы с переменными класса">
     def get_data(self) -> dict:
         """
         :return: словарь с метаданными последовательности спрайтов
         """
         return self.__data
-
-    def set_cooldown(self, value: int):
-        """
-        Устанавливает задержку в смене кадров анимации спрайта
-        :param value: значение в мс, через какое время должна произойти смена кадра анимации спрайта
-        """
-        self.__cooldown = value
-    # </editor-fold>
